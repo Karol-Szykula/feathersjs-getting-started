848d6ade587ff94008c2063aabfc0cca12fd5fb4
Karol-Szykula <karol.szykula@wp.pl>
2018-12-14 23:57:34 +0100

    Add Messages class

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
-const feathers = require('@feathersjs/feathers')
-const app = feathers()
+const feathers = require('@feathersjs/feathers');
 
-app.use('todos', {
-    async get(name) {
-
-        return {
-            name,
-            text: `You have to do ${name}`
-        }
+class Messages {
+    constructor() {
+        this.messages = [];
+        this.currentId = 0;
     }
-})
 
-async function getTodo(name) {
+    async find(params) {
+        // Return the list of all messages
+        return this.messages;
+    }
 
-    const service = app.service('todos')
-    const todo = await service.get(name)
+    async get(id, params) {
+        // Find the message by id
+        const message = this.messages.find(message => message.id === parseInt(id, 10));
 
-    console.log(todo)
+        // Throw an error if it wasn't found
+        if (!message) {
+            throw new Error(`Message with id ${id} not found`);
+        }
+
+        // Otherwise return the message
+        return message;
+    }
+
+    async create(data, params) {
+        // Create a new object with the original data and an id
+        // taken from the incrementing `currentId` counter
+        const message = Object.assign({
+            id: ++this.currentId
+        }, data);
+
+        this.messages.push(message);
+
+        return message;
+    }
+
+    async patch(id, data, params) {
+        // Get the existing message. Will throw an error if not found
+        const message = await this.get(id);
+
+        // Merge the existing message with the new data
+        // and return the result
+        return Object.assign(message, data);
+    }
+
+    async remove(id, params) {
+        // Get the message by id (will throw an error if not found)
+        const message = await this.get(id);
+        // Find the index of the message in our message array
+        const index = this.messages.indexOf(message);
+
+        // Remove the found message from our array
+        this.messages.splice(index, 1);
+
+        // Return the removed message
+        return message;
+    }
 }
 
-getTodo('dishes')
+const app = feathers();
+
+// Initialize the messages service by creating
+// a new instance of our class
+app.use('messages', new Messages());
